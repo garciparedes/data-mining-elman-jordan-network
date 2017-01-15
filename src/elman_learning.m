b9466770c7dcc742e2b883a5c5ad51fd56e08ae2
Sergio Garcia <sergio@garciparedes.me>
2017-01-15 13:07:22 +0100

    Improved code

--- src/elman_learning.m
+++ src/elman_learning.m
@@ -1,4 +1,4 @@
-function [L, W, S, dw_g, O] = elman_learning(L, W, S, dw_g, I);
+function [L, W, S, dw_g] = elman_learning(L, W, S, dw_g, I);
   % L = layers
   % W = weights
   % S = shapes
@@ -7,5 +7,4 @@ function [L, W, S, dw_g, O] = elman_learning(L, W, S, dw_g, I);
     [L, O] = elman_forward(L, W, S, I(i));
     [W, dw_g, e] = elman_backward(L, W, S, dw_g, I(i+1));
   end;
-  [L, O] = elman_forward(L, W, S, I(end));
 end;
--- src/main.m
+++ src/main.m
@@ -1,7 +1,8 @@
 function main()
   file_route = './../dataset/iberdrola.csv';
   impact_range = 20;
-  shape = [1, 32, 32, 1];
+  hold_out_rate = 2/3;
+  shape = [1, 32, 1];
   layers = generate_layers(shape);
   weights = generate_weights(shape);
 
@@ -12,23 +13,34 @@ function main()
     dw_g{i} = zeros(size(weights{i},1), size(weights{i},2));
   end;
 
-  [L, T] = generate_hold_out(size(Y)-(impact_range + 1), 2/3);
-  count = 0;
+  [L, T] = generate_hold_out(size(Y)-(impact_range + 1), hold_out_rate);
   for i = L;
+    M = Y(i:i+impact_range);
+    vecN = M ./ norma;
+    [layers, weights, shape, dw_g] = elman_learning(layers, weights, shape, dw_g, vecN);
+  end;
+
+  count = 0;
+  it = 0;
+  for i = L;
+    it += 1;
     M = Y(i:i+impact_range);
 
     vecN = M ./ norma;
 
-    [layers, weights, shape, dw_g, O] = elman_learning(layers, weights, shape, dw_g, vecN);
+    for j = 1:size(vecN,1)-1;
+      [layers, O] = elman_forward(layers, weights, shape, vecN(j));
+    end;
+    [layers, O] = elman_forward(layers, weights, shape, vecN(end));
+
     vecD = O .* norma;
 
     if (abs(vecD - M(size(M,1))) < 0.05);
       count += 1;
-      result = count / (size(Y,1)-(impact_range + 1))
+      result = count / it
     end;
   end;
 end;
-
 
 clear all;
 main();
